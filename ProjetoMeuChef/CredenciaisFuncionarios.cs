6bceec42ff7d37f6e3ba6a437b30c53ebfe91bf2
Arthur H. <arthurhpb7@icloud.com>
2026-03-25 19:22:32 -0300

    ATIVIDADE COMPLETA!

---
+++ ProjetoMeuChef/CredenciaisFuncionarios.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoMeuChef
+{
+    public class CredenciaisFuncionarios
+    {
+        public string funcionario { get; set; }
+        public string senha { get; set; }
+        public string cargo { get; set; }
+
+        public CredenciaisFuncionarios(string NomeFuncionario, string SenhaFuncionario, string Funcao)
+        {
+            funcionario = NomeFuncionario;
+            senha = SenhaFuncionario;
+            cargo = Funcao;
+        }
+
+        public bool CredenciaisAjudante()
+        {
+            return funcionario == "Fábio" && senha == "123" && cargo == "Ajudante";
+        }
+    }
+}
---
+++ ProjetoMeuChef/frmTelaLoginSecundaria.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoMeuChef
+{
+    public partial class frmTelaLoginSecundaria : Form
+    {
+        public frmTelaLoginSecundaria()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEntreAqui_Click(object sender, EventArgs e)
+        {
+            string NomeFuncionario = txtNomeFuncionario.Text;
+            string SenhaFuncionario = txtSenhaFuncionario.Text;
+            string Funcao = cbFuncionarios.Text;
+
+            CredenciaisFuncionarios cf = new CredenciaisFuncionarios(NomeFuncionario, SenhaFuncionario, Funcao);
+
+            if (cf.CredenciaisAjudante())
+            {
+
+                frmTelaLogin telaLogin = new frmTelaLogin();
+                telaLogin.Show();
+                this.Visible = false;
+
+            }
+
+            else
+            {
+                MessageBox.Show("Credenciais incorretas. Tente novamente!");
+            }
+            
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
